1dad570745b467ac3860170b98acc7562e74c844
Steve Harsant <stevenharsant@gmail.com>
2020-09-20 16:05:01 +0100

    added script

---
+++ Start-AppsWithPrivileges.ps1
@@ -0,0 +1,11 @@
+# Super simple script to start apps from a folder with admin rights.
+# This was created to have Windows Task Scheduler run this script on
+# startup and load a group of apps that need admin rights.
+
+param ($startPath)
+
+$apps = Get-ChildItem -Path $startPath
+
+foreach ($app in $apps) {
+  Start-Process $app
+}
