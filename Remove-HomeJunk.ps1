b96fcc42bf8cac716fa0753b2e713ba19910c848
Steve Harsant <stevenharsant@gmail.com>
2020-09-22 16:33:43 +0100

    Simplified remove junk script

--- Remove-HomeJunk.ps1
+++ Remove-HomeJunk.ps1
@@ -1,22 +1,5 @@
 # Quick script to delete junk directories from my home directory
-
 $directoryNames = '3D Objects', 'Contacts', 'Favorites', 'Links', 'Music', 'Saved Games', 'Searches', 'Videos'
-
 foreach ($directory in $directoryNames) {
-    if(Test-Path -Path "C:\Users\Steveh\$directory")
-    {
-        Remove-Item -Force -Path "C:\Users\Steveh\$directory" -ErrorAction SilentlyContinue | Out-Null
-
-        if(Test-Path -Path "C:\Users\Steveh\$directory")
-        {
-            Write-Host "Failed to delete $directory" -ForegroundColor DarkRed
-        }
-        else
-        {
-            Write-Host "Deleted $directory" -ForegroundColor Green
-        }
-    }
-    else {
-        Write-Host "Nothing to delete" -ForegroundColor Yellow
-    }
+    Remove-Item -Path "$env:USERPROFILE\$directory" -ErrorAction SilentlyContinue | Out-Null
 }
