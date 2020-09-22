f1d0cbe56f92bcf157d3b40f0d65ce4c15d6610d
Steve Harsant <stevenharsant@gmail.com>
2020-09-22 16:37:35 +0100

    added force

--- Remove-HomeJunk.ps1
+++ Remove-HomeJunk.ps1
@@ -1,5 +1,5 @@
 # Quick script to delete junk directories from my home directory
 $directoryNames = '3D Objects', 'Contacts', 'Favorites', 'Links', 'Music', 'Saved Games', 'Searches', 'Videos'
 foreach ($directory in $directoryNames) {
-    Remove-Item -Path "$env:USERPROFILE\$directory" -ErrorAction SilentlyContinue | Out-Null
+    Remove-Item -Path "$env:USERPROFILE\$directory" -Confirm:$false -Force -ErrorAction SilentlyContinue | Out-Null
 }
